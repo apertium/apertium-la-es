efebbf868f828b80d981a62b9f33ac3ce3a515e2
Jim O'Regan <joregan@gmail.com>
2010-02-17 12:41:44 +0000

    more

--- apertium-la-es.la.tsx
+++ apertium-la-es.la.tsx
@@ -32,6 +32,20 @@
     <tags-item lemma="de" tags="pr"/>
     <tags-item lemma="ex" tags="pr"/>
     <tags-item lemma="e" tags="pr"/>
+    <tags-item lemma="palam" tags="pr"/>
+    <tags-item lemma="prae" tags="pr"/>
+    <tags-item lemma="praeter" tags="pr"/>
+    <tags-item lemma="pro" tags="pr"/>
+    <tags-item lemma="procul" tags="pr"/>
+    <tags-item lemma="secundum" tags="pr"/>
+    <tags-item lemma="sine" tags="pr"/>
+    <tags-item lemma="super" tags="pr"/>
+    <tags-item lemma="supra" tags="pr"/>
+    <tags-item lemma="tenus" tags="pr"/>
+    <tags-item lemma="trans" tags="pr"/>
+    <tags-item lemma="ultra" tags="pr"/>
+    <tags-item lemma="usque ad" tags="pr"/>
+    <tags-item lemma="versus" tags="pr"/>
   </def-label> 
   <def-label name="PRACC" closed="true">
     <tags-item lemma="ad" tags="pr"/>
@@ -44,11 +58,30 @@
     <tags-item lemma="contra" tags="pr"/>
     <tags-item lemma="erga" tags="pr"/>
     <tags-item lemma="extra" tags="pr"/>
+    <tags-item lemma="infra" tags="pr"/>
+    <tags-item lemma="inter" tags="pr"/>
+    <tags-item lemma="intra" tags="pr"/>
+    <tags-item lemma="iuxta" tags="pr"/>
+    <tags-item lemma="ob" tags="pr"/>
+    <tags-item lemma="penes" tags="pr"/>
+    <tags-item lemma="per" tags="pr"/>
+    <tags-item lemma="pone" tags="pr"/>
+    <tags-item lemma="post" tags="pr"/>
+    <tags-item lemma="prope" tags="pr"/>
+    <tags-item lemma="propter" tags="pr"/>
+    <tags-item lemma="simul" tags="pr"/>
   </def-label> 
   <def-label name="PRGEN" closed="true">
     <tags-item lemma="causa" tags="pr"/>
     <tags-item lemma="ergo" tags="pr"/>
     <tags-item lemma="gratia" tags="pr"/>
+  </def-label> 
+  <def-label name="PRABLACC" closed="true">
+    <tags-item lemma="sub" tags="pr"/> <!-- Can be either, same translation -->
+    <tags-item lemma="subter" tags="pr"/> <!-- Can be either, same translation -->
+  </def-label> 
+  <def-label name="PRACCGEN" closed="true">
+    <tags-item lemma="tenus" tags="pr"/> <!-- Can be either, same translation -->
   </def-label> 
   <def-label name="IRI" closed="true">
     <tags-item lemma="ire" tags="vblex.inf.psv"/>
