0e6c2d8b7606c3701684b8fad3ae169533e81e9d
Jim O'Regan <joregan@gmail.com>
2010-02-17 12:32:52 +0000

    start categorising prepositions by case while I have the information at hand

--- apertium-la-es.la.tsx
+++ apertium-la-es.la.tsx
@@ -22,6 +22,34 @@
   <def-label name="PR" closed="true">
     <tags-item tags="pr"/>
   </def-label> 
+  <def-label name="PRABL" closed="true">
+    <tags-item lemma="a" tags="pr"/>
+    <tags-item lemma="ab" tags="pr"/>
+    <tags-item lemma="absque" tags="pr"/>
+    <tags-item lemma="clam" tags="pr"/>
+    <tags-item lemma="coram" tags="pr"/>
+    <tags-item lemma="cum" tags="pr"/>
+    <tags-item lemma="de" tags="pr"/>
+    <tags-item lemma="ex" tags="pr"/>
+    <tags-item lemma="e" tags="pr"/>
+  </def-label> 
+  <def-label name="PRACC" closed="true">
+    <tags-item lemma="ad" tags="pr"/>
+    <tags-item lemma="adversus" tags="pr"/>
+    <tags-item lemma="ante" tags="pr"/>
+    <tags-item lemma="apud" tags="pr"/>
+    <tags-item lemma="circa" tags="pr"/>
+    <tags-item lemma="circiter" tags="pr"/>
+    <tags-item lemma="circum" tags="pr"/>
+    <tags-item lemma="contra" tags="pr"/>
+    <tags-item lemma="erga" tags="pr"/>
+    <tags-item lemma="extra" tags="pr"/>
+  </def-label> 
+  <def-label name="PRGEN" closed="true">
+    <tags-item lemma="causa" tags="pr"/>
+    <tags-item lemma="ergo" tags="pr"/>
+    <tags-item lemma="gratia" tags="pr"/>
+  </def-label> 
   <def-label name="IRI" closed="true">
     <tags-item lemma="ire" tags="vblex.inf.psv"/>
   </def-label>
